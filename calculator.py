0cd42c22f4c1504fc1dc1529e21c9fbd6e0763a2
Randall winters <randallwinters@Randalls-iMac-6.local>
2015-10-13 19:19:56 -0500

    banner added

--- calculator.py
+++ calculator.py
@@ -1,4 +1,39 @@
-import Tkinter
+try:
+    import Tkinter  
+except ImportError:
+    import tkinter
 
-class calculator:
+class Calculator:
+	def __init__(self, master):
+		self.master = master
+		self.mainframe = tkinter.Frame(self.master, bg='white')
+		self.mainframe.pack(fill=tkinter.BOTH, expand=True)
+
+		self.build_grid()
+		self.build_banner()
+
+
+	def build_grid(self):
+		self.mainframe.columnconfigure(0, weight=1)
+		self.mainframe.rowconfigure(0, weight=0)
+		self.mainframe.rowconfigure(1, weight=1)
+		self.mainframe.rowconfigure(2, weight=0)
+
+	def build_banner(self):
+		banner = tkinter.Label(
+			self.mainframe,
+			background='black',
+			text='Calculator',
+			fg='blue',
+			font=('Courier', 22)
+			)
+		banner.grid(
+			row=0, column=0,
+			sticky='ew'
+			)
+
+if __name__ == '__main__':
+	root = tkinter.Tk()
+	Calculator(root)
+	root.mainloop()
 	
